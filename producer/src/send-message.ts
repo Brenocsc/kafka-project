a74adc3ea01892c41b8d767da6d9bc43049bd6e3
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-02-08 15:27:06 -0300

    feat: producer on send-message

--- producer/src/send-message.ts
+++ producer/src/send-message.ts
@@ -1,18 +1,19 @@
 import express from 'express'
-import { producer } from './kafka-producer'
+import { kafkaClient } from './kafka-client'
 
 const router = express.Router()
+const producer = kafkaClient.producer()
 
 router.post('/', async (req, res) => {
-  // await producer.connect()
-  // await producer.send({
-  //   topic: 'test-topic',
-  //   messages: [
-  //     { value: 'Hello KafkaJS user!' },
-  //   ],
-  // })
+  await producer.connect()
+  await producer.send({
+    topic: 'test',
+    messages: [
+      { value: req.body.message },
+    ],
+  })
 
-  console.log('done')
+  console.log('Sended message:', { value: req.body.message })
   res.send('CREATED')
 })
 
