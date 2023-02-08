9c856ecc989adc0891d2a070ddf6ce61ade88578
Brenocsc <breno.csc.2013@hotmail.com.br>
2023-02-08 15:28:19 -0300

    feat: use bodyParser in app and call receiveMessage

--- producer/src/index.ts
+++ producer/src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import { router } from './send-message'
+import bodyParser from 'body-parser'
+import { receiveMessage } from './receive-message';
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
 
 app.use(router)
 
 app.listen(port, () => {
   console.log(`Kafka Producer listening on port ${port}`)
 })
+
+receiveMessage(1, 'g1');
+receiveMessage(2, 'g1');
