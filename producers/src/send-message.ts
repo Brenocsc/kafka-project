942a20edbf8038a1ab3cd63b4cd813d934fb6709
Victor Hugo Batista <victor.batista@banqi.com>
2023-03-20 11:18:12 -0300

    Produce an object with random data. Change the event object key to message

--- producers/src/send-message.ts
+++ producers/src/send-message.ts
@@ -5,16 +5,20 @@ const router = express.Router()
 const producer = kafkaClient.producer()
 
 class MessageBody {
-  event: string
+  message: object
 
-  constructor(event: string) {
-    this.event = event;
+  constructor(event: object) {
+    this.message = event;
   }
 }
 
 router.post('/', async (req, res) => {
   await producer.connect()
-  const messageBody = new MessageBody(req.body.message)
+  const messageBody = new MessageBody({
+    a: Math.random(),
+    b: Math.random(),
+    c: Math.random(),
+  });
   await producer.send({
     topic: 'test',
     messages: [
